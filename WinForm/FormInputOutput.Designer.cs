a357c3d442ca0b5479b3a808331a7c1b4256819f
PedroPort3s <pedrodportes@hotmail.com>
2024-05-23 19:29:39 -0300

    feat: manages Win forms UI elements to use SignalR
    installs signalR in WinForms project

--- WinForm/FormInputOutput.Designer.cs
+++ WinForm/FormInputOutput.Designer.cs
@@ -28,82 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnSubmit = new Button();
-            txtInput = new TextBox();
-            txtOutput = new TextBox();
-            lbInput = new Label();
+            txtServerAddress = new TextBox();
+            btnConnect = new Button();
+            lbServerIp = new Label();
+            pnlInputOutput = new Panel();
+            listBoxOutput = new ListBox();
             lbOutput = new Label();
+            lbInput = new Label();
+            txtInput = new TextBox();
+            btnSubmit = new Button();
+            btnDisconnect = new Button();
+            pnlInputOutput.SuspendLayout();
             SuspendLayout();
             // 
-            // btnSubmit
+            // txtServerAddress
             // 
-            btnSubmit.Location = new Point(222, 95);
-            btnSubmit.Name = "btnSubmit";
-            btnSubmit.Size = new Size(75, 23);
-            btnSubmit.TabIndex = 0;
-            btnSubmit.Text = "Submit";
-            btnSubmit.UseVisualStyleBackColor = true;
-            btnSubmit.Click += this.btnSubmit_Click;
+            txtServerAddress.Location = new Point(36, 24);
+            txtServerAddress.Name = "txtServerAddress";
+            txtServerAddress.Size = new Size(191, 23);
+            txtServerAddress.TabIndex = 5;
             // 
-            // txtInput
+            // btnConnect
             // 
-            txtInput.Location = new Point(22, 30);
-            txtInput.Multiline = true;
-            txtInput.Name = "txtInput";
-            txtInput.Size = new Size(275, 59);
-            txtInput.TabIndex = 1;
+            btnConnect.Location = new Point(233, 24);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(75, 23);
+            btnConnect.TabIndex = 7;
+            btnConnect.Text = "Connect";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
             // 
-            // txtOutput
+            // lbServerIp
             // 
-            txtOutput.Location = new Point(22, 132);
-            txtOutput.Multiline = true;
-            txtOutput.Name = "txtOutput";
-            txtOutput.Size = new Size(275, 111);
-            txtOutput.TabIndex = 2;
+            lbServerIp.AutoSize = true;
+            lbServerIp.Location = new Point(36, 6);
+            lbServerIp.Name = "lbServerIp";
+            lbServerIp.Size = new Size(133, 15);
+            lbServerIp.TabIndex = 8;
+            lbServerIp.Text = "Server address with port";
             // 
-            // lbInput
+            // pnlInputOutput
             // 
-            lbInput.AutoSize = true;
-            lbInput.Location = new Point(22, 12);
-            lbInput.Name = "lbInput";
-            lbInput.Size = new Size(35, 15);
-            lbInput.TabIndex = 3;
-            lbInput.Text = "Input";
+            pnlInputOutput.Controls.Add(listBoxOutput);
+            pnlInputOutput.Controls.Add(lbOutput);
+            pnlInputOutput.Controls.Add(lbInput);
+            pnlInputOutput.Controls.Add(txtInput);
+            pnlInputOutput.Controls.Add(btnSubmit);
+            pnlInputOutput.Dock = DockStyle.Bottom;
+            pnlInputOutput.Location = new Point(0, 53);
+            pnlInputOutput.Name = "pnlInputOutput";
+            pnlInputOutput.Size = new Size(435, 376);
+            pnlInputOutput.TabIndex = 10;
+            // 
+            // listBoxOutput
+            // 
+            listBoxOutput.FormattingEnabled = true;
+            listBoxOutput.HorizontalScrollbar = true;
+            listBoxOutput.ItemHeight = 15;
+            listBoxOutput.Location = new Point(36, 133);
+            listBoxOutput.Name = "listBoxOutput";
+            listBoxOutput.Size = new Size(353, 214);
+            listBoxOutput.TabIndex = 11;
             // 
             // lbOutput
             // 
             lbOutput.AutoSize = true;
-            lbOutput.Location = new Point(22, 114);
+            lbOutput.Location = new Point(36, 115);
             lbOutput.Name = "lbOutput";
             lbOutput.Size = new Size(45, 15);
-            lbOutput.TabIndex = 4;
+            lbOutput.TabIndex = 9;
             lbOutput.Text = "Output";
+            // 
+            // lbInput
+            // 
+            lbInput.AutoSize = true;
+            lbInput.Location = new Point(36, 13);
+            lbInput.Name = "lbInput";
+            lbInput.Size = new Size(35, 15);
+            lbInput.TabIndex = 8;
+            lbInput.Text = "Input";
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(36, 31);
+            txtInput.Multiline = true;
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(353, 59);
+            txtInput.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(314, 96);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(75, 23);
+            btnSubmit.TabIndex = 5;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // btnDisconnect
+            // 
+            btnDisconnect.Location = new Point(314, 24);
+            btnDisconnect.Name = "btnDisconnect";
+            btnDisconnect.Size = new Size(75, 23);
+            btnDisconnect.TabIndex = 11;
+            btnDisconnect.Text = "Disconnect";
+            btnDisconnect.UseVisualStyleBackColor = true;
+            btnDisconnect.Click += btnDisconnect_Click;
             // 
             // FormInputOutput
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(309, 255);
-            Controls.Add(lbOutput);
-            Controls.Add(lbInput);
-            Controls.Add(txtOutput);
-            Controls.Add(txtInput);
-            Controls.Add(btnSubmit);
+            ClientSize = new Size(435, 429);
+            Controls.Add(btnDisconnect);
+            Controls.Add(pnlInputOutput);
+            Controls.Add(lbServerIp);
+            Controls.Add(btnConnect);
+            Controls.Add(txtServerAddress);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             Name = "FormInputOutput";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Input and Output";
+            FormClosing += FormInputOutput_FormClosing;
+            pnlInputOutput.ResumeLayout(false);
+            pnlInputOutput.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button btnSubmit;
-        private TextBox txtInput;
-        private TextBox txtOutput;
-        private Label lbInput;
+        private TextBox txtServerAddress;
+        private Button btnConnect;
+        private Label lbServerIp;
+        private Panel pnlInputOutput;
         private Label lbOutput;
+        private Label lbInput;
+        private TextBox txtOutput;
+        private TextBox txtInput;
+        private Button btnSubmit;
+        private ListBox listBoxOutput;
+        private Button btnDisconnect;
     }
 }
--- WinForm/FormInputOutput.cs
+++ WinForm/FormInputOutput.cs
@@ -1,10 +1,151 @@
+using System.Net.Sockets;
+using Microsoft.AspNetCore.SignalR.Client;
+using Models;
+
 namespace WinForm
 {
     public partial class FormInputOutput : Form
     {
+        private HubConnection _connection;
+        private string senderUser = $"WinForms-{Guid.NewGuid().ToString().Substring(0, 7)}";
+
         public FormInputOutput()
         {
             InitializeComponent();
+            EnableChatComponents(false);
+        }
+
+        private async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtInput.Text))
+                {
+                    throw new ArgumentException("Add a text to send to the server.");
+                }
+
+                await SendMessageAsync(txtInput.Text);
+
+                txtInput.Text = string.Empty;
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                MessageBox.Show(argNullException.Message, "Empty message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Unknown error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnConnect_Click(object whoSent, EventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(txtServerAddress.Text))
+                {
+                    throw new FormatException("Invalid IP/Port, try another one.");
+                }
+
+                Uri uri = new Uri(txtServerAddress.Text);
+
+                string serverEndpointWithIpAndPort = uri.AbsoluteUri;
+
+                await ConnectToChatServerAsync(serverEndpointWithIpAndPort);
+
+                _connection.On<ChatMessage>("ReceiveMessage", UpdateReceivedText);
+
+                MessageBox.Show($"Connected successfully stablished with server {serverEndpointWithIpAndPort}!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                EnableChatComponents(true);
+            }
+            catch (FormatException formatException)
+            {
+                MessageBox.Show(formatException.Message, "IP Conversion error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Unknown error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateReceivedText(ChatMessage message)
+        {
+            if (listBoxOutput.InvokeRequired)
+            {
+                listBoxOutput.Invoke(new Action(() => listBoxOutput.Items.Add(message.ToString())));
+            }
+            else
+            {
+                listBoxOutput.Items.Add(message.ToString());
+            }
+        }
+
+        private async Task DisconnectAsync()
+        {
+            if (_connection != null)
+            {
+                await _connection.DisposeAsync();
+                _connection = null;
+            }
+        }
+
+        private async Task SendMessageAsync(string messageText)
+        {
+            if (_connection.State == HubConnectionState.Connected)
+            {
+                ChatMessage message = new ChatMessage
+                {
+                    Content = messageText,
+                    Sender = senderUser,
+                    Timestamp = DateTime.Now
+                };
+
+                await _connection.SendAsync("SendMessage", message);
+            }
+            else
+            {
+                MessageBox.Show("Unable to send messages to server, try connecting again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                EnableChatComponents(false);
+                await DisconnectAsync();
+            }
+        }
+
+        private void EnableChatComponents(bool shouldBeEnabled)
+        {
+            pnlInputOutput.Enabled = shouldBeEnabled;
+            btnDisconnect.Enabled = shouldBeEnabled;
+            btnConnect.Enabled = !shouldBeEnabled;
+
+            if (!shouldBeEnabled)
+            {
+                txtInput.Text = string.Empty;
+                listBoxOutput.Items.Clear();
+            }
+        }
+
+        private async Task ConnectToChatServerAsync(string serverUrl)
+        {
+            serverUrl += "chat";
+
+            _connection = new HubConnectionBuilder()
+            .WithUrl(serverUrl)
+            .Build();
+
+            await _connection.StartAsync();
+        }
+
+        private async void FormInputOutput_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            await DisconnectAsync();
+        }
+
+        private async void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            EnableChatComponents(false);
+            await DisconnectAsync();
+            MessageBox.Show($"Disconnected successfully from the server!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
