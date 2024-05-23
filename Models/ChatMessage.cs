285e8d5a5cd0a4b63e38a3787acab67bbe7526fb
PedroPort3s <pedrodportes@hotmail.com>
2024-05-23 00:55:36 -0300

    feat: creates Models project with common message class

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class ChatMessage
+    {
+        public string Sender { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Sender} says: {Content}\n{Timestamp}";
+        }
+    }
+}
