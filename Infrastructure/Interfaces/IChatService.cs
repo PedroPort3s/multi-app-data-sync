b8f38dd216060ab0acbaf4912b0300ba35989b21
PedroPort3s <pedrodportes@hotmail.com>
2024-05-23 23:47:50 -0300

    feat: moves chat service to created Infrastructure project
    installs SignalR dependencies

---
+++ Infrastructure/Interfaces/IChatService.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IChatService
+    {
+        Task ConnectAsync(string url);
+        Task SendMessageAsync(string senderName, string message);
+        Task OnReceiveMessage(Action<ChatMessage> onMessageReceived);
+    }
+}
---
+++ Infrastructure/Services/ChatService.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using Models;
+
+namespace Infrastructure.Services
+{
+    public class ChatService : IChatService
+    {
+        private HubConnection connection;
+
+        public ChatService()
+        {
+
+        }
+
+        public async Task ConnectAsync(string url)
+        {
+            connection = new HubConnectionBuilder()
+                .WithUrl(url)
+                .Build();
+
+            await connection.StartAsync();
+        }
+
+        public async Task SendMessageAsync(string senderName, string message)
+        {
+            ChatMessage chatMessage = new ChatMessage
+            {
+                Content = message,
+                Sender = senderName,
+                Timestamp = DateTime.Now
+            };
+
+            await connection.SendAsync("SendMessage", chatMessage);
+        }
+
+        public async Task OnReceiveMessage(Action<ChatMessage> onMessageReceived)
+        {
+            connection.On<ChatMessage>("ReceiveMessage", (message) => onMessageReceived(message));
+
+            if (connection.State != HubConnectionState.Connected)
+            {
+                await connection.StartAsync();
+            }
+        }
+    }
+}
